644b128feb827295d3af422177388d3ef43c226d
frannuaire <kferrandon@gmail.com>
2017-10-17 19:16:47 +0200

    complete search with add or update keywords

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -11,7 +11,7 @@ use AppBundle\Entity\Link;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
+use \DateTime;
 class SearchController extends Controller {
 
     /**
@@ -35,39 +35,57 @@ class SearchController extends Controller {
         $param = explode('=', $request->getRequestUri());
         $keywords = $param[1];
         if (null !== $keywords) {
-          //  $lstWords = explode('+', $keywords);
-           $keywords = preg_replace('/\+/', '%', $keywords);
-            // $qb = $this->getDoctrine()->createQueryBuilder(Link::class);
+            $keywords = preg_replace('/\+/', '%', $keywords);
             $em = $this->getDoctrine()->getRepository(Link::class);
 
-      $qb =  $em->createQueryBuilder('l');          
-            $query =$qb->where($qb->expr()->orX(
+            $qb = $em->createQueryBuilder('l');
+            $query = $qb->where($qb->expr()->orX(
                                     $qb->expr()->like('l.name', ':keyword')
-                            ))
-                 /*   $qb->expr()->orX(
-                                    $qb->expr()->like('l.description', ':keydesc')
-                            )
-                    ),*/
+                    ))
                     ->orWhere($qb->expr()->like('l.url', ':keydesc'))
-                    ->setParameter('keyword', '%'.$keywords.'%')
-                    ->setParameter('keydesc', '%'.$keywords.'%')
+                    ->setParameter('keyword', '%' . $keywords . '%')
+                    ->setParameter('keydesc', '%' . $keywords . '%')
                     ->getQuery();
-
-
 
             $websites = $query->getResult();
             $nbResult = count($query->getScalarResult());
-             $keywords = preg_replace('/\%/', ' ', $keywords);
-           // die(var_dump()));
+            $keywords = preg_replace('/\%/', ' ', $keywords);
+
+            $key = $this->getDoctrine()->getRepository(Keyword::class)
+                    ->findBy(array('word' => $keywords));
+
+            $emKey = $this->getDoctrine()->getManager();
+            if (count($key) > 0) {
+                //  die(var_dump($key[0]->getOccurence()+1));
+
+                $key[0]->setOccurence($key[0]->getOccurence() + 1);
+                if ($nbResult > 0) {
+                    $key[0]->setHasResult(1);
+                }
+                $emKey->persist($key[0]);
+                $emKey->flush();
+            } else {
+                $key = new Keyword();
+                $key->setDate(new DateTime());
+                if ($nbResult > 0) {
+                    $key->setHasresults(1);
+                } else {
+                    $key->setHasresults(0);
+                }
+                $key->setOccurence(1);
+                $key->setWord($keywords);
+                $emKey->persist($key);
+                $emKey->flush();
+            }
+            // die(var_dump()));
             return $this->render('search/index.html.twig', [
                         'webSites' => $websites,
-                'keys'=>$keywords,
-                'nbRes'=>$nbResult,
+                        'keys' => $keywords,
+                        'nbRes' => $nbResult,
             ]);
         } else {
             return $this->redirectToRoute('homepage');
         }
-
     }
 
 }
