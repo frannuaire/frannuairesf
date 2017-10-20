b7c4bc06cca40b28f823e4918551173514b48c0c
frannuaire <kferrandon@gmail.com>
2017-10-20 18:06:13 +0200

    add badge when child elements

---
+++ src/AppBundle/Repository/CategoryRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use \Doctrine\ORM\EntityRepository;
+use AppBundle\Entity\Category;
+
+/**
+ * Description of CategoryRepository
+ *
+ * @author kferrandon
+ */
+class CategoryRepository extends EntityRepository {
+
+    protected $lastQuery;
+
+    public function findWithSubCategory($id) {
+
+        $qb = $this->createQueryBuilder('cat')
+                ->select('cat.id, cat.name, cat.root, cat.usable, t2.id as childId, t2.root as childroot ')
+                ->addSelect('
+(select count(c.name) from ' . Category::class . ' c where c.root=cat.id) child ')
+                //   . ' (select count(*) from Category::class c where c.root=cat.id) child ')
+                ->leftJoin(Category::class, 't2', 'WITH', 't2.id=cat.root')
+                ->where('cat.root= :id')
+                ->setParameter('id', $id)
+                //  ->setMaxResults(20)
+                ->getQuery();
+        $this->setLastQuery($qb);
+
+        return $qb->getResult();
+    }
+
+    /**
+     * use this after each select query
+     * @param Query $lastQuery
+     */
+    public function setLastQuery($lastQuery) {
+        $this->lastResult = $lastQuery;
+        return $this;
+    }
+
+    /**
+     * return last number query result elements
+     * @return int
+     */
+    public function countLastResultElements(): int {
+        return count($this->lastResult->getScalarResult());
+    }
+
+}
