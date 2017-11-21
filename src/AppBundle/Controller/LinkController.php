4afaf6c8e1dd6035e810184a804493bca2c6e14b
frannuaire <kferrandon@gmail.com>
2017-11-21 08:48:35 +0100

    add hit on link click

--- src/AppBundle/Controller/LinkController.php
+++ src/AppBundle/Controller/LinkController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use AppBundle\Entity\Link;
 use Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\JsonResponse;
 
 class LinkController extends Controller {
 
@@ -25,23 +26,24 @@ class LinkController extends Controller {
     }
 
     /**
-     * @Route("/api/links/hits/{id}/update", name="hits-update")
+     * @Route("/api/links/hits/update/{id}", name="hits-update")
      */
     public function hitsUpdateAction(Request $request, int $id) {
+
         $website = $this->getDoctrine()
                 ->getRepository(Link::class)
                 ->findOneBy(array('id' => $id), array('date' => 'desc', 'prio' => 'desc'));
-
+        //  var_dump($website);die;
         if ($website instanceof Link) {
             $website->setHits($website->getHits() + 1);
             $em = $this->getDoctrine()->getManager();
             $em->persist($website);
             $em->flush();
+            $serializedEntity = $this->container->get('serializer')->serialize($website, 'json');
 
-            return new Response('Updated Hit', Response::HTTP_OK);
-        }
-        else{
-            return new Response('Oh oh something wrong happened', Response::HTTP_BAD_REQUEST);
+            return new Response($serializedEntity);
+        } else {
+            return new JsonResponse('Oh oh something wrong happened', Response::HTTP_BAD_REQUEST);
         }
     }
 
