69d0eb111142ccca4ce8759408e3d852c1f0b6a0
frannuaire <kferrandon@gmail.com>
2017-11-22 11:45:06 +0100

    add rating message

--- src/AppBundle/Controller/LinkController.php
+++ src/AppBundle/Controller/LinkController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use AppBundle\Entity\Link;
+use AppBundle\Entity\Iplog;
 use Symfony\Component\HttpFoundation\Response;
 use \Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -30,11 +31,32 @@ class LinkController extends Controller {
      */
     public function hitsUpdateAction(Request $request, int $id) {
 
+        $ip = $request->getClientIp();
+        if ($ip == 'unknown') {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
         $website = $this->getDoctrine()
                 ->getRepository(Link::class)
                 ->findOneBy(array('id' => $id), array('date' => 'desc', 'prio' => 'desc'));
-        //  var_dump($website);die;
-        if ($website instanceof Link) {
+
+        $ipLog = $this->getDoctrine()
+                ->getRepository(Iplog::class)
+                ->findOneBy(array('linkid' => $id, 'ip' => $ip));
+        if ($ipLog instanceof Iplog) {
+            $modif = false;
+        } else {
+            $ipLog = new Iplog();
+            $modif = true;
+        }
+
+        $ipLog->setDate(new \DateTime());
+        $ipLog->setIp($ip);
+        $ipLog->setClic(1);
+        $ipLog->setLinkid($website->getId());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($ipLog);
+        $em->flush();
+        if ($website instanceof Link && $modif) {
             $website->setHits($website->getHits() + 1);
             $em = $this->getDoctrine()->getManager();
             $em->persist($website);
@@ -43,31 +65,64 @@ class LinkController extends Controller {
 
             return new Response($serializedEntity);
         } else {
-            return new JsonResponse('Oh oh something wrong happened', Response::HTTP_BAD_REQUEST);
+            $serializedEntity = $this->container->get('serializer')->serialize('Oh oh something wrong happened', 'json');
+            return new Response($serializedEntity);
         }
     }
 
-        /**
+    /**
      * @Route("/api/links/rating/update/{id}", name="rating-update")
      */
     public function ratingUpdateAction(Request $request, int $id) {
-$rating = $request->request->get('rating');
+        $rating = $request->request->get('rating');
+        $ip = $request->getClientIp();
+        if ($ip == 'unknown') {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
 
         $website = $this->getDoctrine()
                 ->getRepository(Link::class)
                 ->findOneBy(array('id' => $id));
-        //  var_dump($website);die;
-        if ($website instanceof Link) {
+
+        $ipLog = $this->getDoctrine()
+                ->getRepository(Iplog::class)
+                ->findOneBy(array('linkid' => $id, 'ip' => $ip));
+        if ($ipLog instanceof Iplog) {
+            if ($ipLog->getVote() == 0) {
+                $modif = true;
+            } else {
+                $modif = false;
+            }
+        } else {
+            $ipLog = new Iplog();
+            $modif = true;
+        }
+
+        $ipLog->setDate(new \DateTime());
+        $ipLog->setIp($ip);
+        $ipLog->setVote($rating);
+        $ipLog->setLinkid($website->getId());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($ipLog);
+        $em->flush();
+
+        if ($website instanceof Link && $modif) {
             $website->setVote($website->getVote() + 1);
-            $website->setRatingValue($website->getRatingValue()+$rating);
+            $website->setRatingValue($website->getRatingValue() + $rating);
             $em = $this->getDoctrine()->getManager();
             $em->persist($website);
             $em->flush();
-            $serializedEntity = $this->container->get('serializer')->serialize($website, 'json');
+            $serializedEntity = $this->container->get('serializer')
+                    ->serialize(array('message' => $this->get('translator')
+                    ->trans('rating.thanks')), 'json');
 
             return new Response($serializedEntity);
         } else {
-            return new JsonResponse('Oh oh something wrong happened', Response::HTTP_BAD_REQUEST);
+            $serializedEntity = $this->container->get('serializer')
+                    ->serialize(array('message' => $this->get('translator')
+                    ->trans('rating.alreadydid')), 'json');
+            return new Response($serializedEntity);
         }
     }
+
 }
